7597097efcf5628bbade6b80328ad5e6ec0ccde0
Paul Fortin <paul@cp-soft.com>
2021-01-12 23:12:23 -0500

    Added install tools as

--- a15cli.go
+++ a15cli.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -29,6 +30,47 @@ func main() {
 				Aliases: []string{"i"},
 				Usage:   `Install some other useful tools: (httpie, jq, slack-cli, yamllint, yq)`,
 				Action:  func(c *cli.Context) error {
+
+					fmt.Println("Installing `httpie`")
+					out, err := exec.Command("brew", "install", "httpie").Output()
+					if err != nil {
+						fmt.Printf("%s", err)
+					}
+					fmt.Println(string(out))
+
+					fmt.Println("Installing `jq`")
+					out, err = exec.Command("brew", "install", "jq").Output()
+					if err != nil {
+						fmt.Printf("%s", err)
+					}
+					fmt.Println(string(out))
+
+					//brew tap rockymadden/rockymadden; brew install rockymadden/rockymadden/slack-cli
+					fmt.Println("Installing `slack-cli`")
+					out, err = exec.Command("brew", "tap", "rockymadden/rockymadden").Output()
+					if err != nil {
+						fmt.Printf("%s", err)
+					}
+					fmt.Println(string(out))
+					out, err = exec.Command("brew", "install", "rockymadden/rockymadden/slack-cli").Output()
+					if err != nil {
+						fmt.Printf("%s", err)
+					}
+					fmt.Println(string(out))
+
+					fmt.Println("Installing `yamllint`")
+					out, err = exec.Command("brew", "install", "yamllint").Output()
+					if err != nil {
+						fmt.Printf("%s", err)
+					}
+					fmt.Println(string(out))
+
+					fmt.Println("Installing `yq`")
+					out, err = exec.Command("brew", "install", "yq").Output()
+					if err != nil {
+						fmt.Printf("%s", err)
+					}
+					fmt.Println(string(out))
 					return nil
 				},
 			},
